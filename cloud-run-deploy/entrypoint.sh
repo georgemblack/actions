1e6702f983f2ba4bebd2f7eaac1f7729002e6a33
georgemblack <contact@georgeblack.me>
2020-01-19 18:49:48 -0600

    Add options to cloud run deploy

--- cloud-run-deploy/entrypoint.sh
+++ cloud-run-deploy/entrypoint.sh
@@ -11,17 +11,26 @@ sanitize() {
 }
 
 sanitize "${INPUT_SERVICENAME}" "serviceName"
-sanitize "${INPUT_GCLOUDPROJECTID}" "gcloudProjectId"
-sanitize "${INPUT_GCLOUDSERVICEACCOUNT}" "gcloudServiceAccount"
-sanitize "${INPUT_GCLOUDRUNTIMESERVICEACCOUNT}" "gcloudRuntimeServiceAccount"
+sanitize "${INPUT_PROJECTID}" "projectId"
+sanitize "${INPUT_SERVICEACCOUNT}" "serviceAccount"
+sanitize "${INPUT_RUNTIMESERVICEACCOUNT}" "runtimeServiceAccount"
 sanitize "${GCLOUD_AUTH}" "GCLOUD_AUTH"
+
+# Set defaults
+SERVICE_NAME=${INPUT_SERVICENAME}
+PROJECT_ID=${INPUT_PROJECTID}
+SERVICE_ACCOUNT=${INPUT_SERVICEACCOUNT}
+RUNTIME_SERVICE_ACCOUNT=${INPUT_RUNTIMESERVICEACCOUNT}
+REGION=${INPUT_REGION:='us-central1'}
+CONCURRENCY=${INPUT_CONCURRENCY:='20'}
+MAX_INSTANCES=${INPUT_MAXINSTANCES:='200'}
 
 # Get version from timestamp
 # Format: YYYYMMDDHHMMSS
 PACKAGE_VERSION=$(date "+%Y%m%d%H%M%S")
 
 # Set project
-gcloud config set project ${INPUT_GCLOUDPROJECTID}
+gcloud config set project ${PROJECT_ID}
 
 # Auth w/service account
 echo ${GCLOUD_AUTH} | base64 --decode > ./key.json
@@ -32,15 +41,15 @@ rm ./key.json
 gcloud builds submit \
   --gcs-log-dir gs://georgeblack-meta/cloud-build/logs \
   --gcs-source-staging-dir gs://georgeblack-meta/cloud-build/source \
-  --tag gcr.io/${INPUT_GCLOUDPROJECTID}/${INPUT_SERVICENAME}:${PACKAGE_VERSION}
+  --tag gcr.io/${PROJECT_ID}/${SERVICE_NAME}:${PACKAGE_VERSION}
 
-# Deploy
-gcloud run deploy ${INPUT_SERVICENAME} \
-  --concurrency 20 \
-  --max-instances 200 \
+# Deploy to Cloud Run
+gcloud run deploy ${SERVICE_NAME} \
+  --concurrency ${CONCURRENCY} \
+  --max-instances ${MAX_INSTANCES} \
   --memory 256Mi \
   --platform managed \
   --allow-unauthenticated \
-  --service-account ${INPUT_GCLOUDRUNTIMESERVICEACCOUNT} \
-  --region us-central1 \
-  --image gcr.io/${INPUT_GCLOUDPROJECTID}/${INPUT_SERVICENAME}:${PACKAGE_VERSION}
+  --service-account ${RUNTIME_SERVICE_ACCOUNT} \
+  --region ${REGION} \
+  --image gcr.io/${PROJECT_ID}/${SERVICE_NAME}:${PACKAGE_VERSION}
